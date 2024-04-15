53036f30128155da37afa0b4be2d1bcfa27fc8ab
Rylie <rylie.stone2005@gmail.com>
2024-04-15 09:59:35 -0400

    made touches to run a server

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // require your server and launch it here
+const server = require('./api/server')
+const port = 9000
+
+server.listen(port, () => {
+    console.log('server has started on port 9000')
+})
